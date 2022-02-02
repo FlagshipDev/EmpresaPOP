8e47a2de743c4d5a12a6854f9d996e7d52a6fdb8
FlagshipDev <68760931+FlagshipDev@users.noreply.github.com>
2022-02-02 13:31:38 +0100

    Fixed an error on main path

--- Server/src/main/java/application/controller/MainController.java
+++ Server/src/main/java/application/controller/MainController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MainController {
 
-    @GetMapping("/api")
+    @GetMapping("/")
     public String greetings() {
         return "Empresa POP -  API Running";
     }
