ab52685b64ac9ba4715e0647bd94a1ff22020fec
aandradeb <aandradeb@fpcoruna.afundacion.org>
2022-02-07 23:09:35 +0100

    Align buttons to center in menu and change menu's size

--- Client/src/main/java/menu/Menu.java
+++ Client/src/main/java/menu/Menu.java
@@ -1,35 +1,39 @@
 package menu;
 
-import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.gui2.*;
 import com.googlecode.lanterna.gui2.dialogs.MessageDialog;
 import com.googlecode.lanterna.gui2.dialogs.MessageDialogButton;
 import com.googlecode.lanterna.screen.Screen;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.TimeZone;
 
+
+/**
+ * Creates a text-based GUI menu using lanterna library (https://github.com/mabe02/lanterna)
+ *
+ * @Author aandradeb
+ */
 public class Menu {
 
-    private DefaultTerminalFactory terminalFactory;
-    private Screen screen;
+    private DefaultTerminalFactory terminalFactory; // Auto-detection for figuring out which terminal create
+    private Screen terminal; // Save the terminal implementation detected
     private WindowBasedTextGUI textGUI;
-    private Window window;
+    private Window menuOptions;
     private Panel contentPanel;
-    private GridLayout gridLayout;
+    private LinearLayout linearLayout;
 
     public Menu() {
         try {
-            this.terminalFactory = new DefaultTerminalFactory();
-            this.screen = terminalFactory.createScreen();
-            this.textGUI = new MultiWindowTextGUI(screen);
-            this.window = new BasicWindow("Menu options");
-            this.contentPanel = new Panel(new GridLayout(2));
-            this.gridLayout = (GridLayout) contentPanel.getLayoutManager();
+            this.terminalFactory = new DefaultTerminalFactory()
+                    .setTerminalEmulatorTitle("EmpresaPOP")
+                    .setInitialTerminalSize(new TerminalSize(100, 42));
+            this.terminal = terminalFactory.createScreen();
+            this.textGUI = new MultiWindowTextGUI(terminal);
+            this.contentPanel = new Panel(new LinearLayout());
+            this.linearLayout = (LinearLayout) contentPanel.getLayoutManager();
+
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -37,7 +41,7 @@ public class Menu {
 
     public void launch() {
         try {
-            screen.startScreen();
+            terminal.startScreen();
             showMenu();
         } catch (IOException e) {
             e.printStackTrace();
@@ -46,34 +50,46 @@ public class Menu {
     }
 
     private void showMenu() {
+        this.menuOptions = new BasicWindow("Menu options");
+        this.menuOptions.setFixedSize(new TerminalSize(60, 8));
+
+        Button btnAddEmployee = new Button("Insertar empleado", () -> insertNewEmployee());
+        btnAddEmployee.setLayoutData(LinearLayout.createLayoutData(LinearLayout.Alignment.Center));
+
+        Button btnDeleteEmployee = new Button("Borrar empleado", () -> deleteEmployee());
+        btnDeleteEmployee.setLayoutData(LinearLayout.createLayoutData(LinearLayout.Alignment.Center));
+
+        Button btnSeeAllEmployeers = new Button("Visualizar todos los empleados", () -> displayAllEmployees());
+        btnSeeAllEmployeers.setLayoutData(LinearLayout.createLayoutData(LinearLayout.Alignment.Center));
+
+        Button btnSeeAllDepartments = new Button("Visualizar todos los departamentos", () -> displayAllDepartments());
+        btnSeeAllDepartments.setLayoutData(LinearLayout.createLayoutData(LinearLayout.Alignment.Center));
+
+        Button btnExit = new Button("Salir", menuOptions::close);
+        btnExit.setLayoutData(LinearLayout.createLayoutData(LinearLayout.Alignment.Center));
+
+        contentPanel.addComponent(new EmptySpace());
+        contentPanel.addComponent(btnAddEmployee);
+        contentPanel.addComponent(btnDeleteEmployee);
+        contentPanel.addComponent(btnSeeAllEmployeers);
+        contentPanel.addComponent(btnSeeAllDepartments);
+        contentPanel.addComponent(new EmptySpace());
+        contentPanel.addComponent(btnExit);
+
+        menuOptions.setComponent(contentPanel);
+        textGUI.addWindowAndWait(this.menuOptions);
+
         try {
-            contentPanel.addComponent(
-                    new EmptySpace()
-                            .setLayoutData(
-                                    GridLayout.createHorizontallyFilledLayoutData(2)));
-            contentPanel.addComponent(new Button("Insertar empleado", () -> insertNewEmployee()));
-            contentPanel.addComponent(new Button("Borrar empleado", () -> deleteEmployee()));
-            contentPanel.addComponent(new Button("Visualizar todos los empleados", () -> displayAllEmployees()));
-            contentPanel.addComponent(new Button("Visualizar todos los departamentos", () -> displayAllDepartments()));
-            contentPanel.addComponent(new Button("Salir", window::close));
-
-            window.setComponent(contentPanel);
-            textGUI.addWindowAndWait(window);
-
-        } finally {
-            if (screen != null) {
-                try {
-                    screen.stopScreen();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+            if (terminal != null) {
+                terminal.stopScreen();
             }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
     private void insertNewEmployee() {
         MessageDialog.showMessageDialog(textGUI, "MessageBox", "This is a message box", MessageDialogButton.OK);
-
     }
 
     private void deleteEmployee() {}
