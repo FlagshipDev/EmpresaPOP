9b499d49d2f2e7996794fa5bfc1186d3928bdc64
Miguel Jaimes <mjaimesl@fpcoruna.afundacion.org>
2022-02-10 14:32:58 +0100

    feat: Implemented EmployeeController_DeleteEmployeeTest. feat: Implemented EmployeeController_PostEmployeeTest.

--- Server/src/test/java/application/EmployeeController_DeleteEmployeeTest.java
+++ Server/src/test/java/application/EmployeeController_DeleteEmployeeTest.java
@@ -23,41 +23,52 @@ public class EmployeeController_DeleteEmployeeTest {
     @Autowired
     private MockMvc mvc;
 
-    @Before
-    public void postTempEmployee() throws Exception {
-
-    }
-
     // Testing code 200
     @Test
     public void deleteEmployee_succesfulyDeleted() throws Exception {
+        //Create Temp Employee
         JSONObject tempEmployee = new JSONObject();
         tempEmployee.put("empno", 9999);
-        tempEmployee.put("ename", "Pepe de Pura");
-        tempEmployee.put("job", "Presidente");
+        tempEmployee.put("empname", "Pepe");
+        tempEmployee.put("job", "Pdte");
         tempEmployee.put("mgr", 9000);
-        tempEmployee.put("hiredate", new Date());
-        tempEmployee.put("sal", 9000);
-        tempEmployee.put("comm", 9000);
-        tempEmployee.put("deptno", 10);
+        tempEmployee.put("hiredate", "2022-01-01");
+        tempEmployee.put("sal", 9000.0);
+        tempEmployee.put("comm", 9000.0);
+        tempEmployee.put("deptno", 10.0);
         mvc.perform(MockMvcRequestBuilders.post("/employee")
                         .header("AUTH_EMPRESAPOP", AppTest.AUTH_TOKEN)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(tempEmployee.toString())
                         .accept(MediaType.APPLICATION_JSON))
                 .andDo(print())
-                .andExpect(status().isCreated());
+                .andExpect(status().isOk());
+        //Delete Employee
+        mvc.perform(MockMvcRequestBuilders.delete("/employee/" +
+                                tempEmployee.getInt("empno"))
+                        .header("AUTH_EMPRESAPOP", AppTest.AUTH_TOKEN)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk());
     }
-
     // Testing code 400
     @Test
-    public void deleteEmployee_IdDontExist() {
-        // !TODO Implement test logic
+    public void deleteEmployee_IdDontExist() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.delete("/employee/9999")
+                        .header("AUTH_EMPRESAPOP", AppTest.AUTH_TOKEN)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().is(400));
     }
+
 
     // Testing code 403
     @Test
-    public void deleteEmployee_Forbidden() {
-        // !TODO Implement test logic
+    public void deleteEmployee_Forbidden() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.delete("/employee/9999")
+                        .header("AUTH_EMPRESAPOP", "")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().is(403));
     }
 }
--- Server/src/test/java/application/EmployeeController_PostEmployeeTest.java
+++ Server/src/test/java/application/EmployeeController_PostEmployeeTest.java
@@ -1,10 +1,16 @@
 package application;
 
+import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -15,19 +21,71 @@ public class EmployeeController_PostEmployeeTest {
 
     // Testing code 201
     @Test
-    public void postEmployee_successfullyCreated() {
-        // !TODO Implement test logic
+    public void postEmployee_successfullyCreated() throws Exception {
+        //Create Temp Employee
+        JSONObject tempEmployee = new JSONObject();
+        tempEmployee.put("empno", 9999);
+        tempEmployee.put("empname", "Pepe");
+        tempEmployee.put("job", "Pdte");
+        tempEmployee.put("mgr", 9000);
+        tempEmployee.put("hiredate", "2022-01-01");
+        tempEmployee.put("sal", 9000.0);
+        tempEmployee.put("comm", 9000.0);
+        tempEmployee.put("deptno", 10.0);
+        mvc.perform(MockMvcRequestBuilders.post("/employee")
+                        .header("AUTH_EMPRESAPOP", AppTest.AUTH_TOKEN)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(tempEmployee.toString())
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk());
+        //Delete Employee
+        mvc.perform(MockMvcRequestBuilders.delete("/employee/" +
+                                tempEmployee.getInt("empno"))
+                        .header("AUTH_EMPRESAPOP", AppTest.AUTH_TOKEN)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk());
     }
-
     // Testing code 400
     @Test
-    public void postEmployee_IdDontExist() {
-        // !TODO Implement test logic
+    public void postEmployee_IdDontExist() throws Exception {
+        //Create Temp Employee
+        JSONObject tempEmployee = new JSONObject();
+        tempEmployee.put("empno", "abcd");
+        tempEmployee.put("empname", "Pepe");
+        tempEmployee.put("job", "Pdte");
+        tempEmployee.put("mgr", 9000);
+        tempEmployee.put("hiredate", "2022-01-01");
+        tempEmployee.put("sal", 9000.0);
+        tempEmployee.put("comm", 9000.0);
+        tempEmployee.put("deptno", 10.0);
+        mvc.perform(MockMvcRequestBuilders.post("/employee")
+                        .header("AUTH_EMPRESAPOP", AppTest.AUTH_TOKEN)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(tempEmployee.toString())
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().is(400));
     }
-
     // Testing code 403
     @Test
-    public void postEmployee_Forbidden() {
-        // !TODO Implement test logic
+    public void postEmployee_Forbidden() throws Exception {
+        //Create Temp Employee
+        JSONObject tempEmployee = new JSONObject();
+        tempEmployee.put("empno", 9999);
+        tempEmployee.put("empname", "Pepe");
+        tempEmployee.put("job", "Pdte");
+        tempEmployee.put("mgr", 9000);
+        tempEmployee.put("hiredate", "2022-01-01");
+        tempEmployee.put("sal", 9000.0);
+        tempEmployee.put("comm", 9000.0);
+        tempEmployee.put("deptno", 10.0);
+        mvc.perform(MockMvcRequestBuilders.post("/employee")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(tempEmployee.toString())
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().is(403));
     }
 }
